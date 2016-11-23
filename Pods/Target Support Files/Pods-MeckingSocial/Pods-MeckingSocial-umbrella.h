b7f04be4adfa818164594d622034888be6e34923
Steve Mecking <stevo@Steves-MacBook-Air.local>
2016-11-23 04:08:44 -0700

    lesson 144

---
+++ Pods/Target Support Files/Pods-MeckingSocial/Pods-MeckingSocial-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MeckingSocialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MeckingSocialVersionString[];
+
