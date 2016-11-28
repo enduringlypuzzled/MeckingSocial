9f7362cdf1e9fc232696faa20cd0c2cb9ef82be9
Steve Mecking <stevo@Steves-MacBook-Air.local>
2016-11-28 10:06:09 -0700

    Firebase get completed

--- MeckingSocial/FeedVC.swift
+++ MeckingSocial/FeedVC.swift
@@ -12,8 +12,9 @@ import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    
     @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +22,26 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource=self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            //print(snapshot.value)
+            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                        
+                    }
+                    
+                }
+            }
+            self.tableView.reloadData()
         })
         
-            
+        
     }
     
     // TableView Stuff -------------------------------------------
@@ -34,11 +51,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //return UITableViewCell()
+        
+        let post = posts[indexPath.row]
+        print("Steve: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
